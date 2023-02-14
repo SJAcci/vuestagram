709ca93c7e93ebaf5eda32b3e8cdccad3df3ce53
SJAcci <sungjuan1006@gmail.com>
2023-02-14 09:18:40 +0900

    vuestagram

---
+++ src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Post :feed="a" v-for="(a,i) in feed" :key="i"/>
+    </div>
+</template>
+<script>
+import Post from './Post.vue';
+export default {
+    // eslint-disable-next-line
+    name: 'Container',
+    props: {
+        feed: Array,
+    },
+    components: {
+        Post,
+    }
+}
+</script>
+<style>
+    
+</style
